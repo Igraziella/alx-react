a62eb49aecaf9142108f48cc16be9d57ada36bd5
igraziella <igraziella2@gmail.com>
2023-07-25 00:22:53 +0100

    capitalized dashboard

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 // Add three different paragraphs to the page
 $(document).ready(() => {
-    const paragraph1 = $('<p>').text('Holberton dashboard');
+    const paragraph1 = $('<p>').text('Holberton Dashboard');
     const paragraph2 = $('<p>').text('Dashboard data for the students');
     const paragraph3 = $('<p>').text('Copyright - Holberton School');
     $('body').append(paragraph1, paragraph2, paragraph3);
