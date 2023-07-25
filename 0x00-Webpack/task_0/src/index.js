7ed7ef09e139c696b804196b4fc68e17ac42c6f7
igraziella <igraziella2@gmail.com>
2023-07-25 13:32:01 +0100

    added paragraphs to the page

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,9 +1,7 @@
 import $ from 'jquery';
 
 // Add three different paragraphs to the page
-$(document).ready(() => {
-    const paragraph1 = $('<p>').text('Holberton Dashboard');
-    const paragraph2 = $('<p>').text('Dashboard data for the students');
-    const paragraph3 = $('<p>').text('Copyright - Holberton School');
-    $('body').append(paragraph1, paragraph2, paragraph3);
-});
+"$('body').append('<p>Holberton Dashboard</p>');", 
+"$('body').append('<p>Dashboard data for the students</p>');", 
+"$('body').append('<p>Copyright - Holberton School</p>');"
+
