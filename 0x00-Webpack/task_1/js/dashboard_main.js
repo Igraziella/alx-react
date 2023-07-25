d9b450085ab64b6ba42e6468e353d8597211fd9e
igraziella <igraziella2@gmail.com>
2023-07-25 14:44:56 +0100

    using webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,11 +1,19 @@
 import $ from 'jquery';
+import _ from 'lodash';
 
 // Add paragraph elements to the page
-$(document).ready(() => {
-    const p1 = $('<p>').text('Holberton Dashboard');
-    const p2 = $('<p>').text('Dashboard data for the students');
-    const button = $('button').text('Click here to get started');
-    const p3 = $(<p id='count'></p>);
-    const p4 = $('<p>').text('Copyright - Holberton School');
-    $('body').append(p1, p2, button, p3, p4);
-});
+"$('body').append('<p>Holberton Dashboard</p>');", 
+"$('body').append('<p>Dashboard data for the students</p>');", 
+"$('body').append('<button id='clickButtton'>Click here to get started</button>');"
+"$('body').append('<p> id='count'</p>')"
+"$('body').append('<p>Copyright - Holberton School</p>');"
+
+// Get the number of times the button is clicked
+let count = 0;
+const updateCounter = () => {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+};
+
+const debouncedUpdateCounter = _.debounce(updateCounter, 300);
+$('#clickButton').on('click', debouncedUpdateCounter);
