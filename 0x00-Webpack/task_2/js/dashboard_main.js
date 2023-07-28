688a571ea09f471fd03d4177856cfc9afe6b984d
igraziella <igraziella2@gmail.com>
2023-07-28 17:18:20 +0100

    minor changes to dashboard

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -20,7 +20,3 @@ const $button = $("<button>Click here to get started</button>").on(
     "click",
     _.debounce( updateCounter, 500, { leading: true, trailing: false })
   );
-  
-  $('body').append("<p>Dashboard data for the students</p>");
-  $('body').append($button);
-  $('body').append("<p id='count'></p>");
