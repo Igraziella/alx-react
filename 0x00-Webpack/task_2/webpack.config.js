9a31442220e49f6e6f143ff03e13518ebf14a633
igraziella <igraziella2@gmail.com>
2023-07-28 11:57:14 +0100

    minor modifications to config file

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,24 +2,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  mode: 'production',
-  module: {
-    rules: [
-        ...
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-            type: 'asset/resource',
-            use: ['file-loader', {
-                loader: 'image-webpack-loader'
+    mode: 'production',
+    entry: path.resolve(__dirname, './js/dashboard_main.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+                use: ["file-loader", {
+                    loader: "image-webpack-loader" 
             }]
         },
         ]
