9cdb73781b9638be937935bbc71d8fcb420e28eb
igraziella <igraziella2@gmail.com>
2023-08-05 11:46:07 +0100

    npm project using webpack

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,31 @@
+// Setup a system to output a bundle.js file in dist folder.
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    module: { 
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+                use: ["file-loader", {
+                    loader: "image-webpack-loader"
+                }]
+            },
+            ]
+        },
+};
