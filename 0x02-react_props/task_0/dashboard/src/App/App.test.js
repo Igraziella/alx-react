e2d535c1ad99e15cd1f31e15d6b797e11cec8ff1
igraziella <igraziella2@gmail.com>
2023-08-11 13:34:40 +0100

    modified file

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -1,28 +1,7 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render } from 'testing-library/react';
 import App from './App';
 
-describe('App component', () => {
-  it('renders without crashing', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.exists()).toBe(true);
-  });
-
-  it('renders a div with the class App-header', () => {
-    const wrapper = mount(<App />);
-    const appHeader = wrapper.find('[data-testid="app-header"]');
-    expect(appHeader.exists()).toBe(true);
-  });
-
-  it('renders a div with the class App-body', () => {
-    const wrapper = mount(<App />);
-    const appBody = wrapper.find('[data-testid="app-body"]');
-    expect(appBody.exists()).toBe(true);
-  });
-
-  it('renders a div with the class App-footer', () => {
-    const wrapper = mount(<App />);
-    const appFooter = wrapper.find('[data-testid="app-footer"]');
-    expect(appFooter.exists()).toBe(true);
-  });
+test('App component renders without crashing', () => {
+  render(<App />);
 });
