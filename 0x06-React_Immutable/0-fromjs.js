dc58691db63d301a4fa6a7661ad184f87f2984e2
igraziella <igraziella2@gmail.com>
2023-08-17 00:02:01 -0700

    task 0

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,5 @@
-import { fromJS } from 'immutable';
+import { fromJS, Map } from 'immutable';
 
-export default function getImmutableObject(object) => {
-  const myImmutableMap = fromJS({fear: true, smell: -1033575916.9145899, wall: false, thing: -914767132})
+export default function getImmutableObject(object) {
+  return fromJS(object);
 }
