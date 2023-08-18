80b3085d9784efbd4890866e43c976bfad20952c
igraziella <igraziella2@gmail.com>
2023-08-18 00:26:20 -0700

    task 2

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,20 +1,15 @@
+import { Map } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
   if (object === undefined || array.length === 0) {
     return undefined;
   }
 
-  let currentValue = object;
+  const immutableMap = Map(object);
+  const value = immutableMap.getIn(array);
 
-  for (const jey of array) {
-    if (typeof currentValue === 'object' && currentValue !== null) {
-      currentValue = currentValue[key];
-    } else {
-      return undefined;
-    } 
-  }
-
-  if (typeof currentValue === 'string' || currentValue instanceof Map) {
-    return currentValue;
+  if (typeof value === 'string' || value instanceof Map) {
+    return value;
   } 
 
   return undefined;
